d215ab451c1ea2006334c0bf49f406426779bbfe
Ruoxi Wang <rwang3@seas.upenn.edu>
2025-07-17 22:28:25 +0000

    Final submission: Destination.sol in root

---
+++ Destination.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./BridgeToken.sol";
+
+contract Destination is AccessControl {
+    bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
+
+    mapping(address => address) public underlying_tokens;
+    mapping(address => address) public wrapped_tokens;
+    address[] public tokens;
+
+    event Creation(address indexed underlying_token, address indexed wrapped_token);
+    event Wrap(address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount);
+    event Unwrap(address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount);
+
+    constructor(address admin) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(CREATOR_ROLE, admin);
+        _grantRole(WARDEN_ROLE, admin);
+    }
+
+    function createToken(address _underlying_token, string memory name, string memory symbol) 
+        public onlyRole(CREATOR_ROLE) returns(address) 
+    {
+        require(underlying_tokens[_underlying_token] == address(0), "Token already registered");
+
+        // Deploy the BridgeToken with Destination contract as the admin (minter)
+        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+        address wrappedTokenAddress = address(newToken);
+
+        underlying_tokens[_underlying_token] = wrappedTokenAddress;
+        wrapped_tokens[wrappedTokenAddress] = _underlying_token;
+        tokens.push(_underlying_token);
+
+        emit Creation(_underlying_token, wrappedTokenAddress);
+        return wrappedTokenAddress;
+    }
+
+    function wrap(address _underlying_token, address _recipient, uint256 _amount) 
+        public onlyRole(WARDEN_ROLE) 
+    {
+        address wrappedToken = underlying_tokens[_underlying_token];
+        require(wrappedToken != address(0), "Underlying token not registered");
+
+        BridgeToken(wrappedToken).mint(_recipient, _amount);
+
+        emit Wrap(_underlying_token, wrappedToken, _recipient, _amount);
+    }
+
+    function unwrap(address _wrapped_token, address _recipient, uint256 _amount) public {
+        require(wrapped_tokens[_wrapped_token] != address(0), "Wrapped token not recognized");
+
+        BridgeToken token = BridgeToken(_wrapped_token);
+        require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
+
+        token.burnFrom(msg.sender, _amount);
+
+        address underlying = wrapped_tokens[_wrapped_token];
+        emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
+    }
+}
+
+
+
+
